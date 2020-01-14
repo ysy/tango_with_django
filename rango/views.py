4878cc7398eb9ba2c4edb2dbdd5b0e593008dcef
YuanSuyi <yuansuyi@hzdusun.com>
2020-01-14 16:17:44 +0800

    Templates

--- rango/views.py
+++ rango/views.py
@@ -2,4 +2,14 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Rango says hi there parter")
+    dict = {
+        "blodmessage" : "Curnchy, creamy, cookies, candy, cpucake!"
+    }
+    return render(request, 'rango/index.html', context=dict)
+
+def about(request):
+    return HttpResponse("""
+    
+    <h1> Rango says here is about page </h1>
+     <a href=/rango><h1>Main Page</h1></a>
+    """)
--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
@@ -53,10 +52,12 @@ MIDDLEWARE_CLASSES = [
 
 ROOT_URLCONF = 'tango_with_django_project.urls'
 
+TEMPLATES_DIR = os.path.join(BASE_DIR, "templates")
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR, ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -64,11 +65,17 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },
 ]
 
+STATIC_DIR = os.path.join(BASE_DIR, "static")
+MEDIA_DIR = os.path.join(BASE_DIR, "media")
+
+
+STATICFILES_DIRS = [STATIC_DIR, ]
 WSGI_APPLICATION = 'tango_with_django_project.wsgi.application'
 
 
@@ -120,3 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = MEDIA_DIR
+print(MEDIA_ROOT)
+MEDIA_URL = '/media/'

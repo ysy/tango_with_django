b65ccb758d31e7061b94a761bcdf5446be91a1bb
YuanSuyi <yuansuyi@hzdusun.com>
2020-01-16 10:56:25 +0800

    commit missing files.

--- rango/urls.py
+++ rango/urls.py
@@ -10,5 +10,5 @@ urlpatterns = [
     url(r'^register/$', views.register, name='register'),
     url(r'^login/$', views.user_login, name='login'),
     url(r'^logout/$', views.user_logout, name='logout'),
-
+    url(r'^restricted/$', views.restricted, name='restricted'),
 ]
